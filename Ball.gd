fb6e783600f5549ac55290fef100d517e4326596
Pablo <raistlin_majere_11@hotmail.com>
2023-10-23 11:36:24 +0200

    Señal colisiones
    Añadida señal para colisiones de la bola (no va)

--- Ball.gd
+++ Ball.gd
@@ -1,6 +1,15 @@
 extends RigidBody2D
 
-var velocity = Vector2(100, 100)
+var velocity = Vector2(100, 0)
 
 func _process(delta):
 	move_and_collide(velocity*delta)
+
+
+func _on_body_entered(body):
+	if body.is_in_group("walls"):
+		velocity.x = -velocity.x
+		print("Colisión con los muros detectada")
+	if body.is_in_group("players"):
+		velocity.x = -velocity.x
+		print("Colisión con los jugadores detectada")
